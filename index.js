cdfdeaaa2f02c404a7fc42d5d2eeeb76dc524105
jammie-commits <jay.mbugua.ph@gmail.com>
2024-03-30 18:11:07 +0300

    complete lab

--- index.js
+++ index.js
@@ -11,6 +11,20 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+// const titleCased = () => {
+//   return tutorials
+
+
+function titleCased() {
+  return tutorials.map(tutorial => {
+    const words = tutorial.split(' ');
+
+    const titleCasedWords = words.map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    });
+
+    return titleCasedWords.join(' ');
+  });
 }
+
+
